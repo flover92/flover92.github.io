e6b6eb2f24c1e37355518ebafbaef40df6e0d86f
flover92 <flover10@i.ua>
2017-01-05 17:27:19 +0400

    first commit

--- exercise1.php
+++ exercise1.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+ <head>
+	 <meta chartset="utf-8"/>
+	 <title>Завдання 1</title>
+	 <link href="" rel="stylesheet" type="text/css">
+	 <link rel="shortcut icon" href="img/softgroup.ico"/>
+ </head>
+<body>
+<div class="if"><strong>Завдання 1</strong><br/>
+У форму вводиться число N. Знайти всі досконалі числа до N.
+Досконале число - це таке число, що дорівнює сумі всіх своїх дільників,
+крім себе самого. Наприклад, число 6 є досконалим, тому що крім себе
+самого ділиться на числа 1, 2 і 3, які в сумі дають 6.</div><br/>
 <form method="GET" action="">
 	<input type="number" name="num" value="6" min="6" requred>
 	<input type="submit" value="send">
@@ -22,7 +36,7 @@
 
 	}
 
-	$perfectNums = [];
+	$perfectNums = array();
 	for($i = 6; $i<=$_GET['num']; $i++) {
 		if(isPerfect($i)) {
 			$perfectNums[] = $i;
@@ -32,3 +46,5 @@
 	if(count($perfectNums) > 0)
 		echo "<p>Perfect numbers from 6 to {$_GET['num']}: ".implode(', ', $perfectNums)."</p>";
 ?>
+</body>
+</html>
--- exercise3.php
+++ exercise3.php
@@ -2,44 +2,42 @@
 <html>
  <head>
 	 <meta chartset="utf-8"/>
-	 <title>Автор</title>
-	 <link href="author.css" rel="stylesheet" type="text/css">
+	 <title>Завдання 3</title>
+	 <link href="" rel="stylesheet" type="text/css">
 	 <link rel="shortcut icon" href="img/softgroup.ico"/>
  </head>
 <body>
-	<?php
-	function plus() 
-{
+<div class="if"><strong>Завдання 3</strong><br/>
+Вводяться N натуральних чисел більше 2. Підрахувати, скільки серед
+них простих чисел.</div><br/>
+<?php
+function plus()
+	{
 ?>
 	<form name="plus" action="" method="GET">
 	<input name="A" type="text">
 	<input name="B" type="text">
-	<input type="submit" value=" click ">
+	<input type="submit" value="find prime numbers">
 	</form>
-<?
- 
-	for($i=$_GET['A'];$i<$_GET['B'];$i++)
-	{
-    if ($i<2)
-    continue;
-    else
-    {
-    $check=true;
-    for($j=2;$j<$i;$j++)
-        {
-        if ($i%$j===0) 
-        $check=false;
-        }
-        if ($check===true)
-        echo $i." ";
-        }
-    }   
-}
-
+	
+<?php
+	for ($i = $_GET['A']; $i < $_GET['B']; $i++)
+		{
+		if ($i < 2) continue;
+		  else
+			{
+			$check = true;
+	for ($j = 2; $j < $i; $j++)
+				{
+		if ($i % $j === 0) $check = false;
+				}
+		if ($check === true) echo $i . " ";
+			}
+		}
+	}
 ?>
 <?php
 echo plus();
 ?>
-
 </body>
 </html>
